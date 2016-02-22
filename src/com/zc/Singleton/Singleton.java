3bcbb9eb241bca7598ecb577128f3e77d7b15c86
zengchao9 <919031564@qq.com>
2016-02-22 17:48:15 +0800

    Update Singleton.java

--- src/com/zc/Singleton/Singleton.java
+++ src/com/zc/Singleton/Singleton.java
@@ -10,6 +10,7 @@ public class Singleton {
     /**
      *    类级的内部类，也就是静态的成员式内部类，该内部类的实例与外部类的实例
      *    没有绑定关系，而且只有被调用到时才会装载，从而实现了延迟加载。
+     *     
      */
     private static class SingletonHolder{
         /**
